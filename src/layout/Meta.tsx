204165eb20f753284bf8d6002b9af50986975e68
Ronny Coste <ronny21.97@gmail.com>
2022-09-01 21:44:56 -0400

    streamline metadata

--- src/layout/Meta.tsx
+++ src/layout/Meta.tsx
@@ -10,88 +10,58 @@ type IMetaProps = {
   canonical?: string;
 };
 
-const Meta = (props: IMetaProps) => {
+const Meta = (props: IMetaProps, customMeta) => {
   const router = useRouter();
+
+  const meta = {
+    title: 'Curated By',
+    description: 'An idea to better use my data starting with Google Maps.',
+    image: '/icon-512x512.png',
+    type: 'website',
+    ...customMeta,
+  };
 
   return (
     <>
       <Head>
-        <meta name="curated by" content="Curated By" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
         <meta
-          name="description"
-          content="An idea to better use my data starting with Google Maps."
+          property="og:url"
+          content={`https://ronnycoste.com${router.asPath}`}
         />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#2B5797" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#000000" />
+        <link rel="canonical" href={`https://ronnycoste.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Ronny Coste" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="Curated By Ronny Coste" />
+        <meta name="twitter:site" content="@costeronny" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
+          href="/apple-touch-icon.png"
         />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/icons/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color="#5bbad5"
+          href="/favicon-16x16.png"
         />
         <link rel="shortcut icon" href="/favicon.ico" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://curatedby.ronnycoste.com" />
-        <meta name="twitter:title" content="Curated By" />
-        <meta
-          name="twitter:description"
-          content="An idea to better use my data starting with Google Maps."
-        />
-        <meta
-          name="twitter:image"
-          content="https://yourdomain.com/icons/android-chrome-192x192.png"
-        />
-        <meta name="twitter:creator" content="@costeronny" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Curated By" />
-        <meta
-          property="og:description"
-          content="An idea to better use my data starting with Google Maps."
-        />
-        <meta property="og:site_name" content="Curated By" />
-        <meta property="og:url" content="https://curatedby.ronnycoste.com" />
-        <meta
-          property="og:image"
-          content="https://curatedby.ronnycoste.com/icons/apple-touch-icon.png"
-        />
 
         <meta charSet="UTF-8" key="charset" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -107,7 +77,6 @@ const Meta = (props: IMetaProps) => {
         />
         <meta name="Curated By" content="Curated By" />
         <meta name="theme-color" content="#000" />
-        <title>Curated By</title>
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="icon"
